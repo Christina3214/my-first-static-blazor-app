1f73268d964e64635b3ec09dc088c75f77bc54eb
Christina Løvenholt Andersen <clan63283@edu.ucl.dk>
2025-03-18 13:52:32 +0100

    c

---
+++ Api/ChatController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Graph;
+using BlazorApp.Shared;
+
+namespace Api
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    internal class ChatController : ControllerBase
+    {
+        private static List<ChatMessage> messages = new List<ChatMessage>();
+        [HttpGet]
+        public IEnumerable<ChatMessage> GetMessages()
+        {
+            return messages.OrderBy(m => m.Timestamp);
+        }
+        [HttpPost]
+        public IActionResult PostMessage(ChatMessage message)
+        {
+            message.Timestamp = DateTime.Now;
+            messages.Add(message);
+            return Ok();
+        }
+        
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,20 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+//korrekt opsætning af HTTP-client
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+//Tilføj CORS politik (hvis du har brug for at konfigurere den her)
+
+
 
 await builder.Build().RunAsync();
